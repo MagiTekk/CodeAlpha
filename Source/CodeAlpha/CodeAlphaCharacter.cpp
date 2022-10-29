4e4e11f1cbda3760f9b04655e05c45448ab20303
Erick Pombo Sonderblohm <sonderblohm.dev@gmail.com>
2022-10-29 02:12:13 +0200

    Moved all character code to CodeAlphaCharacter

--- Source/CodeAlpha/CodeAlphaCharacter.cpp
+++ Source/CodeAlpha/CodeAlphaCharacter.cpp
@@ -7,6 +7,11 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Character/Abilities/CAGameplayAbility.h"
+#include "Character/Abilities/CAAbilitySystemComponent.h"
+#include "Character/Abilities/AttributeSets/CACharacterAttributeSetBase.h"
+#include "Player/CAPlayerState.h"
+#include <GameplayAbilitySpec.h>
 
 //////////////////////////////////////////////////////////////////////////
 // ACodeAlphaCharacter
@@ -49,6 +54,235 @@ ACodeAlphaCharacter::ACodeAlphaCharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+
+	bAlwaysRelevant = true;
+	DeadTag = FGameplayTag::RequestGameplayTag(FName("State.Dead"));
+	EffectRemoveOnDeathTag = FGameplayTag::RequestGameplayTag(FName("State.RemoveOnDeath"));
+}
+
+UAbilitySystemComponent* ACodeAlphaCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent.Get();
+}
+
+bool ACodeAlphaCharacter::IsAlive() const
+{
+	return GetHealth() > 0.0f;
+}
+
+int32 ACodeAlphaCharacter::GetAbilityLevel(EAbilityInputID AbilityID) const
+{
+	return 1;	//#TODO_esonderblohm make ability system progression
+}
+
+void ACodeAlphaCharacter::RemoveCharacterAbilities()
+{
+	if (GetLocalRole() != ROLE_Authority || !AbilitySystemComponent.IsValid() || !AbilitySystemComponent->bCharacterAbilitiesGiven)
+	{
+		return;
+	}
+
+	// Remove any abilities added from a previous call. This checks to make sure the ability is in the startup 'CharacterAbilities' array.
+	TArray<FGameplayAbilitySpecHandle> AbilitiesToRemove;
+	for (const FGameplayAbilitySpec& Spec : AbilitySystemComponent->GetActivatableAbilities())
+	{
+		if ((Spec.SourceObject == this) && CharacterAbilities.Contains(Spec.Ability->GetClass()))
+		{
+			AbilitiesToRemove.Add(Spec.Handle);
+		}
+	}
+
+	// Do in two passes so the removal happens after we have the full list
+	for (int32 i = 0; i < AbilitiesToRemove.Num(); i++)
+	{
+		AbilitySystemComponent->ClearAbility(AbilitiesToRemove[i]);
+	}
+
+	AbilitySystemComponent->bCharacterAbilitiesGiven = false;
+}
+
+void ACodeAlphaCharacter::Die()
+{
+	// Only runs on Server
+	RemoveCharacterAbilities();
+
+	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	GetCharacterMovement()->GravityScale = 0;
+	GetCharacterMovement()->Velocity = FVector(0);
+
+	OnCharacterDied.Broadcast(this);
+
+	if (AbilitySystemComponent.IsValid())
+	{
+		AbilitySystemComponent->CancelAllAbilities();
+
+		FGameplayTagContainer EffectTagsToRemove;
+		EffectTagsToRemove.AddTag(EffectRemoveOnDeathTag);
+		int32 NumEffectsRemoved = AbilitySystemComponent->RemoveActiveEffectsWithTags(EffectTagsToRemove);
+
+		AbilitySystemComponent->AddLooseGameplayTag(DeadTag);
+	}
+
+	if (DeathMontage)
+	{
+		PlayAnimMontage(DeathMontage);
+	}
+	else
+	{
+		FinishDying();
+	}
+}
+
+void ACodeAlphaCharacter::FinishDying()
+{
+	Destroy();
+}
+
+int32 ACodeAlphaCharacter::GetCharacterLevel() const
+{
+	if (AttributeSetBase.IsValid())
+	{
+		return static_cast<int32>(AttributeSetBase->GetCharacterLevel());
+	}
+
+	return 0;
+}
+
+float ACodeAlphaCharacter::GetHealth() const
+{
+	if (AttributeSetBase.IsValid())
+	{
+		return AttributeSetBase->GetHealth();
+	}
+
+	return 0.0f;
+}
+
+float ACodeAlphaCharacter::GetMaxHealth() const
+{
+	if (AttributeSetBase.IsValid())
+	{
+		return AttributeSetBase->GetMaxHealth();
+	}
+
+	return 0.0f;
+}
+
+float ACodeAlphaCharacter::GetMana() const
+{
+	if (AttributeSetBase.IsValid())
+	{
+		return AttributeSetBase->GetMana();
+	}
+
+	return 0.0f;
+}
+
+float ACodeAlphaCharacter::GetMaxMana() const
+{
+	if (AttributeSetBase.IsValid())
+	{
+		return AttributeSetBase->GetMaxMana();
+	}
+
+	return 0.0f;
+}
+
+void ACodeAlphaCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	ACAPlayerState* CAPlayerState = GetPlayerState<ACAPlayerState>();
+	if (CAPlayerState)
+	{
+		// Set the AbilitySystemComponent on the Server. Clients do this in OnRep_PlayerState()
+		AbilitySystemComponent = Cast<UCAAbilitySystemComponent>(CAPlayerState->GetAbilitySystemComponent());
+
+		// AI won't have PlayerControllers so we can init again here just to be sure. No harm in setting up twice for characters that have PlayerControllers.
+		CAPlayerState->GetAbilitySystemComponent()->InitAbilityActorInfo(CAPlayerState, this);
+
+		// Set the AttributeSetBase for convenience attribute functions
+		AttributeSetBase = CAPlayerState->GetAttributeSetBase();
+
+		// #TODO_esonderblohm If we handle players disconnecting and rejoining in the future, we'll have to change this so that possession from rejoining doesn't reset attributes.
+		// For now assume possession = spawn/respawn.
+		InitializeAttributes();
+
+		// -- Respawn specific things that won't affect first possession.
+
+		// Forcibly set the DeadTag count to 0
+		AbilitySystemComponent->SetTagMapCount(DeadTag, 0);
+
+		// Set Health/Mana/Stamina to their max. This is only necessary for *Respawn*.
+		SetHealth(GetMaxHealth());
+		SetMana(GetMaxMana());
+
+		// -- End respawn specific things
+
+		AddStartupEffects();
+		AddCharacterAbilities();
+	}
+}
+
+void ACodeAlphaCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+
+	ACAPlayerState* CAPlayerState = GetPlayerState<ACAPlayerState>();
+	if (CAPlayerState)
+	{
+		// Set the ASC for clients. Server does this in PossessedBy.
+		AbilitySystemComponent = Cast<UCAAbilitySystemComponent>(CAPlayerState->GetAbilitySystemComponent());
+
+		// Init ASC Actor Info for clients. Server will init its ASC when it possesses a new Actor.
+		AbilitySystemComponent->InitAbilityActorInfo(CAPlayerState, this);
+
+		// Bind player input to the AbilitySystemComponent. Also called in SetupPlayerInputComponent because of a potential race condition.
+		BindAbilitySystemComponentInput();
+
+		// Set the AttributeSetBase for convenience attribute functions
+		AttributeSetBase = CAPlayerState->GetAttributeSetBase();
+
+		// If we handle players disconnecting and rejoining in the future, we'll have to change this so that possession from rejoining doesn't reset attributes.
+		// For now assume possession = spawn/respawn.
+		InitializeAttributes();
+
+
+		// -- Respawn specific things that won't affect first possession.
+
+		// Forcibly set the DeadTag count to 0
+		AbilitySystemComponent->SetTagMapCount(DeadTag, 0);
+
+		// Set Health/Mana to their max. This is only necessary for *Respawn*.
+		SetHealth(GetMaxHealth());
+		SetMana(GetMaxMana());
+
+		// -- End respawn specific things
+	}
+}
+
+void ACodeAlphaCharacter::Jump()
+{
+	if (IsAlive())
+	{
+		Super::Jump();
+	}
+}
+
+void ACodeAlphaCharacter::AddControllerPitchInput(float Val)
+{
+	if (IsAlive())
+	{
+		Super::AddControllerPitchInput(Val);
+	}
+}
+
+void ACodeAlphaCharacter::AddControllerYawInput(float Val)
+{
+	if (IsAlive())
+	{
+		Super::AddControllerYawInput(Val);
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -75,11 +309,28 @@ void ACodeAlphaCharacter::SetupPlayerInputComponent(class UInputComponent* Playe
 	// handle touch devices
 	PlayerInputComponent->BindTouch(IE_Pressed, this, &ACodeAlphaCharacter::TouchStarted);
 	PlayerInputComponent->BindTouch(IE_Released, this, &ACodeAlphaCharacter::TouchStopped);
+
+	// Bind player input to the AbilitySystemComponent. Also called in OnRep_PlayerState because of a potential race condition.
+	BindAbilitySystemComponentInput();
+}
+
+void ACodeAlphaCharacter::BindAbilitySystemComponentInput()
+{
+	if (!ASCInputBound && AbilitySystemComponent.IsValid() && IsValid(InputComponent))
+	{
+		AbilitySystemComponent->BindAbilityActivationToInputComponent(InputComponent, FGameplayAbilityInputBinds(FString("ConfirmTarget"),
+			FString("CancelTarget"), FString("EAbilityInputID"), static_cast<int32>(EAbilityInputID::Confirm), static_cast<int32>(EAbilityInputID::Cancel)));
+
+		ASCInputBound = true;
+	}
 }
 
 void ACodeAlphaCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
 {
-	Jump();
+	if (IsAlive())
+	{
+		Jump();
+	}
 }
 
 void ACodeAlphaCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
@@ -89,19 +340,109 @@ void ACodeAlphaCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Lo
 
 void ACodeAlphaCharacter::TurnAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+	if (IsAlive())
+	{
+		// calculate delta for this frame from the rate information
+		AddControllerYawInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+	}
 }
 
 void ACodeAlphaCharacter::LookUpAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+	if (IsAlive())
+	{
+		// calculate delta for this frame from the rate information
+		AddControllerPitchInput(Rate * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+	}
+}
+
+void ACodeAlphaCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ACodeAlphaCharacter::AddCharacterAbilities()
+{
+	// Grant abilities, but only on the server	
+	if (GetLocalRole() != ROLE_Authority || !AbilitySystemComponent.IsValid() || AbilitySystemComponent->bCharacterAbilitiesGiven)
+	{
+		return;
+	}
+
+	for (TSubclassOf<UCAGameplayAbility>& StartupAbility : CharacterAbilities)
+	{
+		AbilitySystemComponent->GiveAbility(
+			FGameplayAbilitySpec(StartupAbility, GetAbilityLevel(StartupAbility.GetDefaultObject()->AbilityInputID), static_cast<int32>(StartupAbility.GetDefaultObject()->AbilityInputID), this));
+	}
+
+	AbilitySystemComponent->bCharacterAbilitiesGiven = true;
+}
+
+void ACodeAlphaCharacter::InitializeAttributes()
+{
+	if (!AbilitySystemComponent.IsValid())
+	{
+		return;
+	}
+
+	if (!DefaultAttributes)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s() Missing DefaultAttributes for %s. Please fill in the character's Blueprint."), *FString(__FUNCTION__), *GetName());
+		return;
+	}
+
+	// Can run on Server and Client
+	FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+	EffectContext.AddSourceObject(this);
+
+	FGameplayEffectSpecHandle NewHandle = AbilitySystemComponent->MakeOutgoingSpec(DefaultAttributes, GetCharacterLevel(), EffectContext);
+	if (NewHandle.IsValid())
+	{
+		FActiveGameplayEffectHandle ActiveGEHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToTarget(*NewHandle.Data.Get(), AbilitySystemComponent.Get());
+	}
+}
+
+void ACodeAlphaCharacter::AddStartupEffects()
+{
+	if (GetLocalRole() != ROLE_Authority || !AbilitySystemComponent.IsValid() || AbilitySystemComponent->bStartupEffectsApplied)
+	{
+		return;
+	}
+
+	FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+	EffectContext.AddSourceObject(this);
+
+	for (TSubclassOf<UGameplayEffect> GameplayEffect : StartupEffects)
+	{
+		FGameplayEffectSpecHandle NewHandle = AbilitySystemComponent->MakeOutgoingSpec(GameplayEffect, GetCharacterLevel(), EffectContext);
+		if (NewHandle.IsValid())
+		{
+			FActiveGameplayEffectHandle ActiveGEHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToTarget(*NewHandle.Data.Get(), AbilitySystemComponent.Get());
+		}
+	}
+
+	AbilitySystemComponent->bStartupEffectsApplied = true;
+}
+
+void ACodeAlphaCharacter::SetHealth(float Health)
+{
+	if (AttributeSetBase.IsValid())
+	{
+		AttributeSetBase->SetHealth(Health);
+	}
+}
+
+void ACodeAlphaCharacter::SetMana(float Mana)
+{
+	if (AttributeSetBase.IsValid())
+	{
+		AttributeSetBase->SetMana(Mana);
+	}
 }
 
 void ACodeAlphaCharacter::MoveForward(float Value)
 {
-	if ((Controller != nullptr) && (Value != 0.0f))
+	if (IsAlive() && (Controller != nullptr) && (Value != 0.0f))
 	{
 		// find out which way is forward
 		const FRotator Rotation = Controller->GetControlRotation();
@@ -115,7 +456,7 @@ void ACodeAlphaCharacter::MoveForward(float Value)
 
 void ACodeAlphaCharacter::MoveRight(float Value)
 {
-	if ( (Controller != nullptr) && (Value != 0.0f) )
+	if (IsAlive() && (Controller != nullptr) && (Value != 0.0f) )
 	{
 		// find out which way is right
 		const FRotator Rotation = Controller->GetControlRotation();
--- Source/CodeAlpha/CodeAlphaCharacter.h
+++ Source/CodeAlpha/CodeAlphaCharacter.h
@@ -4,10 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "CodeAlpha.h"
+#include <AbilitySystemInterface.h>
+#include <GameplayTagContainer.h>
 #include "CodeAlphaCharacter.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCharacterDiedDelegate, ACodeAlphaCharacter*, Character);
+
 UCLASS(config=Game)
-class ACodeAlphaCharacter : public ACharacter
+class ACodeAlphaCharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
@@ -18,14 +23,111 @@ class ACodeAlphaCharacter : public ACharacter
 	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
+
 public:
 	ACodeAlphaCharacter();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	UFUNCTION(BlueprintCallable, Category = "CodeAlpha|Character")
+	virtual bool IsAlive() const;
+
+	// Switch on AbilityID to return individual ability levels.
+	UFUNCTION(BlueprintCallable, Category = "CodeAlpha|Character")
+	virtual int32 GetAbilityLevel(EAbilityInputID AbilityID) const;
+
+	// Removes all CharacterAbilities. Can only be called by the Server. Removing on the Server will remove from Client too.
+	virtual void RemoveCharacterAbilities();
+
+	virtual void Die();
+
+	UFUNCTION(BlueprintCallable, Category = "CodeAlpha|Character")
+	virtual void FinishDying();
+
+	UFUNCTION(BlueprintCallable, Category = "CodeAlpha|Character|Attributes")
+	int32 GetCharacterLevel() const;
+
+	UFUNCTION(BlueprintCallable, Category="CodeAlpha|Character|Attributes")
+	float GetHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "CodeAlpha|Character|Attributes")
+	float GetMaxHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "CodeAlpha|Character|Attributes")
+	float GetMana() const;
+
+	UFUNCTION(BlueprintCallable, Category = "CodeAlpha|Character|Attributes")
+	float GetMaxMana() const;
+
+	UPROPERTY(BlueprintAssignable, Category = "CodeAlpha|Character")
+	FCharacterDiedDelegate OnCharacterDied;
 
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Input)
 	float TurnRateGamepad;
 
+	// Only called on the Server. Calls before Server's AcknowledgePossession.
+	virtual void PossessedBy(AController* NewController) override;
+
+	// Client only
+	virtual void OnRep_PlayerState() override;
+
+	virtual void Jump() override;
+
+	void AddControllerPitchInput(float Val) override;
+
+	void AddControllerYawInput(float Val) override;
+
 protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	TWeakObjectPtr<class UCAAbilitySystemComponent> AbilitySystemComponent;
+	TWeakObjectPtr<class UCACharacterAttributeSetBase> AttributeSetBase;
+
+	FGameplayTag DeadTag;
+	FGameplayTag EffectRemoveOnDeathTag;
+
+	// Is the AbilitySystemComponent input bound or not?
+	bool ASCInputBound = false;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category="CodeAlpha|Character")
+	FText CharacterName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CodeAlpha|Animation")
+	UAnimMontage* DeathMontage;
+
+	// Default abilities for this Character. These will be removed on Character death and regiven if Character respawns.
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "CodeAlpha|Abilities")
+	TArray<TSubclassOf<class UCAGameplayAbility>> CharacterAbilities;
+
+	// Default attributes for a character for initializing on spawn/respawn.
+	// This is an instant GE that overrides the values for attributes that get reset on spawn/respawn.
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "CodeAlpha|Abilities")
+	TSubclassOf<class UGameplayEffect> DefaultAttributes;
+
+	// These effects are only applied one time on startup
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "CodeAlpha|Abilities")
+	TArray<TSubclassOf<class UGameplayEffect>> StartupEffects;
+
+	// Grant abilities on the Server. The Ability Specs will be replicated to the owning client.
+	virtual void AddCharacterAbilities();
+
+	// Initialize the Character's attributes. Must run on Server but we run it on Client too
+	// so that we don't have to wait. The Server's replication to the Client won't matter since
+	// the values should be the same.
+	virtual void InitializeAttributes();
+
+	virtual void AddStartupEffects();
+
+	/**
+	* Setters for Attributes. Only use these in special cases like Respawning, otherwise use a GE to change Attributes.
+	* These change the Attribute's Base Value.
+	*/
+
+	virtual void SetHealth(float Health);
+	virtual void SetMana(float Mana);
 
 	/** Called for forwards/backward input */
 	void MoveForward(float Value);
@@ -55,6 +157,11 @@ protected:
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	// End of APawn interface
+
+	// Called from both SetupPlayerInputComponent and OnRep_PlayerState because of a potential race condition where the PlayerController might
+	// call ClientRestart which calls SetupPlayerInputComponent before the PlayerState is repped to the client so the PlayerState would be null in SetupPlayerInputComponent.
+	// Conversely, the PlayerState might be repped before the PlayerController calls ClientRestart so the Actor's InputComponent would be null in OnRep_PlayerState.
+	void BindAbilitySystemComponentInput();
 
 public:
 	/** Returns CameraBoom subobject **/
